8ce988f79f3fe5fedc8d3c39a2a7513e77a97c80
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-10-09 16:30:35 +0900

    build: remove duplicate publish tasks We don't need the publish task for the ReVanced Library

--- revanced-lib/build.gradle.kts
+++ revanced-lib/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.10"
     alias(libs.plugins.binary.compatibility.validator)
-    `maven-publish`
 }
 
 dependencies {
@@ -29,50 +28,4 @@ kotlin { jvmToolchain(11) }
 
 java {
     withSourcesJar()
-}
-
-publishing {
-    repositories {
-        mavenLocal()
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/revanced/revanced-cli")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("gpr") {
-            from(components["java"])
-
-            version = project.version.toString()
-
-            pom {
-                name = "ReVanced Library"
-                description = "Library containing common utilities for ReVanced"
-                url = "https://revanced.app"
-
-                licenses {
-                    license {
-                        name = "GNU General Public License v3.0"
-                        url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "ReVanced"
-                        name = "ReVanced"
-                        email = "contact@revanced.app"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git://github.com/revanced/revanced-cli.git"
-                    developerConnection = "scm:git:git@github.com:revanced/revanced-cli.git"
-                    url = "https://github.com/revanced/revanced-cli"
-                }
-            }
-        }
-    }
 }
