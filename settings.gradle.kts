c4e2a938a09debbf4593591d04ec754a1e9dbbaf
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-11-26 23:09:42 +0900

    rollback: Move ReVanced Library subproject to another repository https://github.com/ReVanced/revanced-cli/commit/995f2ec99bf91c3f8d0319d7b6af06012ce3b012
    This commit is to apply changes to the ReVanced Library.
    
    Ideally I would create a new ReVanced Library fork and add my changes there.
    In that case, in order to use github actions, there will be the inconvenience of having to release a new fork in Maven Central.
    
    To prevent this I merge ReVanced Library 1.3.0 into ReVanced CLI.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,16 @@
-rootProject.name = "revanced-cli"
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        maven { url = uri("https://jitpack.io") }
+        google()
+    }
+}
 
 buildCache {
     local {
         isEnabled = !System.getenv().containsKey("CI")
     }
 }
+
+include("revanced-cli", "revanced-lib")
