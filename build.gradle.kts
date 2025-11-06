c3a1f256125d074e979536e4b0c4b645227730e1
inotia00 <108592928+inotia00@users.noreply.github.com>
2025-11-06 20:35:40 +0900

    build: Bump gradle and dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.0.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
@@ -25,17 +25,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.20")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.20")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.21")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.21")
     implementation("app.revanced:smali:2.5.3-a3836654")
 
     implementation("io.github.inotia00:revanced-patcher:8.0.2-arsclib-SNAPSHOT")
-    implementation("info.picocli:picocli:4.7.6")
+    implementation("info.picocli:picocli:4.7.7")
     implementation("com.github.revanced:jadb:2531a28109") // updated fork
-    implementation("com.android.tools.build:apksig:8.7.0")
-    implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.0")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.20")
+    implementation("com.android.tools.build:apksig:8.13.0")
+    implementation("org.bouncycastle:bcpkix-jdk18on:1.82")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.5")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.21")
 }
 
 kotlin {
